0cc1bb90e2d7a0c330dffbf57ff4f751a11e8ee5
muhammadrinaldi7 <muhammad.rinaldi007.mr@gmail.com>
2025-10-06 15:21:19 +0800

    Initial Commit:Belajar membuat backend GO

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"rndev/backend-api/config"
+	"rndev/backend-api/database"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+
+	//load config .env
+	config.LoadEnv()
+
+	//inisialisasi database
+	database.InitDB()
+	
+	//inisialiasai Gin
+	router := gin.Default()
+
+	//membuat route dengan method GET
+	router.GET("/", func(c *gin.Context) {
+
+		//return response JSON
+		c.JSON(200, gin.H{
+			"message": "Hello World!",
+		})
+	})
+
+	//mulai server
+	router.Run(":" + config.GetEnv("APP_PORT", "3000"))
+}
